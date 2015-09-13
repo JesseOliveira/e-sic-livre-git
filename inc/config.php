ddbece9b66b85b2009d8228c456ea102fb9c082f
maellson <maellson@hotmail.com>
2015-09-13 11:41:42 -0300

    Pasta original 09-09-2015

---
+++ inc/config.php
@@ -0,0 +1,27 @@
+<?php
+/**********************************************************************************
+ Sistema e-SIC Livre: sistema de acesso a informa��o baseado na lei de acesso.
+ 
+ Copyright (C) 2014 Prefeitura Municipal do Natal
+ 
+ Este programa � software livre; voc� pode redistribu�-lo e/ou
+ modific�-lo sob os termos da Licen�a GPL2.
+***********************************************************************************/
+
+error_reporting(E_ERROR);
+
+define("SISTEMA_NOME", "e-SIC Livre"); //nome do sistema para exibi��o em lugares diversos
+define("SISTEMA_CODIGO", "esiclivre"); //codigo para defini��o da lista de sess�o do sistema
+
+
+define("MAIL_HOST", "you@yourdomain");
+define("DBHOST", "localhost");
+define("DBUSER", "root");
+define("DBPASS", "");
+define("DBNAME", "dbesiclivre");
+
+define("SITELNK", "http://localhost/esiclivre/");	//endere�o principal do site
+
+define("URL_BASE_SISTEMA", "http://localhost/esiclivre/");	//endere�o principal do site
+
+?>
